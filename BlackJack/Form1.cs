52a49d039998556331f1003bf3ac746dc5006986
K9452 <K9452@student.jamk.fi>
2017-03-30 13:40:53 +0300

    2 classia

---
+++ BlackJack/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackJack
+{
+    public partial class Form1 : Form
+    {
+        public Deck currentdeck { get; set; }
+        public Hand player_hand { get; set; }
+        public Hand dealer_hand { get; set; }
+        public int numcards { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Visible = true; // jako button on näkyvä
+            button2.Visible = false; // löynti buttoni on piilotettu
+            button3.Visible = false; // jää buttoni on piilotettu
+            pictureBox1.Visible = true; // näyttää ensimmäisen kortin paikan
+            PictureBox2.Visible = false; // näytetään toinen kortti
+            pictureBox3.visible = false; // piilotetaan loput
+            pictureBox4.visible = false; // piilotetaan loput
+            pictureBox5.visible = false; // piilotetaan loput
+            pictureBox6.visible = false; // piilotetaan loput
+            pictureBox7.visible = false; // piilotetaan loput
+            pictureBox8.visible = false; // piilotetaan loput
+            pictureBox9.visible = false; // piilotetaan loput
+            pictureBox10.visible = false; // piilotetaan loput
+            pictureBox11.visible = false; // piilotetaan loput
+            pictureBox12.visible = false; // piilotetaan loput
+
+
+        }
+
+       
+    }
+}
---
+++ BlackJack/RandomNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+    {
+    public class RandomNumber
+    {
+        private static readonly
+        System.Security.Cryptography.RNGCryptoServiceProvider_seed = 
+        new System.Security.Cryptography.RNGCryptoServiceProvider();
+            publicstatic int NumberBetween(int minimum, int maximum)
+        {
+            byte[] randomNumber = new byte[1];  // uusi byte taulukko
+
+            _seed.GetBytes(randomNumber);
+
+            double asciiValue = Convert.ToDouble(randomNumber[0]);
+
+            double multiplier = Math.Max(0, (asciiValue / 255d) - 0.00000000001d);
+
+            int range = maximum - minimum + 1; // lisätään yksi, jotta pyöristäminen mahdollistuu
+
+            double randomValue = Math.Floor(multiplier * range); // varmistaa että pysytään annetulla alueella
+
+            return (int)(minimum + randomValue); // lisää minimin randomiin
+        }
+    }
+}
